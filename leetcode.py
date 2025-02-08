b5c060ae4f1e22882fbf1cc290576242acea33b0
KasturiJamale <kasturijamale1023@gmail.com>
2025-02-08 00:55:00 -0500

    roman_to_integer

---
+++ leetcode.py
@@ -0,0 +1,17 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        RomanMap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+        result = 0
+        i = 0
+
+        while i < len(s):
+            if i + 1 < len(s) and RomanMap[s[i]] < RomanMap[s[i + 1]]:
+                result += RomanMap[s[i + 1]] - RomanMap[s[i]]
+                i += 1
+
+            else:
+                result += RomanMap[s[i]]
+            i += 1
+
+        return result
