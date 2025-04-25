82315dcc6835e04c1b8ac7c60dc6640f115a8ce8
KasturiJamale <kasturijamale1023@gmail.com>
2025-04-24 23:00:30 -0400

    assign_cookies

---
+++ leetcode.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+
+        g.sort()
+        s.sort()
+
+        matched = 0
+
+        i = 0
+        j = 0
+
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                matched += 1
+                i+=1
+            j+=1
+
+        return matched
